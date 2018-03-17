fd852ec2444b7e8a31ef37372c6ef209c2a8b07d
xxDOOMbox <shawwian@gmail.com>
2018-03-17 16:28:38 -0400

    falling off ledges

--- src/main.js
+++ src/main.js
@@ -26,6 +26,7 @@ function initStage () {
   ctx.fillStyle = "#FF0000";
   ctx.fillRect(pixel.x, pixel.y, pixel.width, pixel.height);
   new Block(0, canvas.height - 100, canvas.width, 100, "#000000");
+  new Block(500, 350, 300, 100, "#000000");
   drawBlocks(); 
 }
 
@@ -64,23 +65,26 @@ function movePixel () {
       pixel.x += pixel.speed; 
     }
   }
+  
+  let collision = detectGroundCollision(pixel);
+  if (collision) {
+    grounded = true;
+    canJump = true;
+    pixel.vely = 0; 
+    pixel.y = collision.y - pixel.height;
+  }  else {
+    grounded = false;
+  }
+
+  if (!grounded) {
+    pixel.y += pixel.vely; 
+    pixel.vely += grav;
+  }
 
   if (grounded && canJump && jump) {
     pixel.vely = -30; 
     canJump = false; 
     grounded = false; 
-  }
-
-  if (!grounded) {
-    pixel.y += pixel.vely; 
-    pixel.vely += grav; 
-
-    if (detectGroundCollision(pixel)) {
-      grounded = true;
-      canJump = true; 
-      pixel.vely = 0; 
-      pixel.y = ground - pixel.height;
-    } 
   }
 }
 
@@ -88,15 +92,14 @@ function detectCollisionX (entity) {
   return false; 
 }
 
-// TODO: finish implementing 
 function detectGroundCollision (entity) {
   const { length } = blocks; 
   for (let i=0; i<length; i++) {
     let block = blocks[i]; 
     if (isBelow(block, entity)) {
-      // check if entity is ABOUT to collide with block 
-      if ((entity.y + entity.height) + entity.vely >= block.y) {
-        return true; 
+      // check if entity is ABOUT to collide with block  
+      if (entity.y + entity.height + entity.vely >= block.y) {
+        return block; 
       }
     }
   }
